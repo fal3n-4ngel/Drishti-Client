8ac8ef62f7cbc17151e002b2badf29a45efd2e2f
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2022-11-11 10:49:39 +0530

    Update contact.dart
    Changed the padding issue
    added all contributors

--- lib/Screens/contact.dart
+++ lib/Screens/contact.dart
@@ -19,38 +19,25 @@ class _MyWidgetState extends State<ContactScreen> {
           child: Container(
             alignment: Alignment.center,
             height: 500,
-            width: MediaQuery.of(context).size.width - 50,
+            width: MediaQuery.of(context).size.width - 20,
             padding: EdgeInsets.all(20),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(
-                "Contact",
-                style: TextStyle(
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    fontFamily: 'Comfortaa',
-                    fontSize: 42,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1),
-              ),
-              Text(
-                "the developers",
-                style: TextStyle(
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    fontFamily: 'Comfortaa',
-                    fontSize: 18,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.w300,
-                    height: 1),
-              ),
-              SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    "Ferwin Lopez",
+                    "Contact",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontFamily: 'Comfortaa',
+                        fontSize: 42,
+                        letterSpacing:
+                            0 /*percentages not used in flutter. defaulting to zero*/,
+                        fontWeight: FontWeight.normal,
+                        height: 1),
+                  ),
+                  Text(
+                    "the developers",
                     style: TextStyle(
                         color: Color.fromARGB(255, 0, 0, 0),
                         fontFamily: 'Comfortaa',
@@ -60,143 +47,158 @@ class _MyWidgetState extends State<ContactScreen> {
                         fontWeight: FontWeight.w300,
                         height: 1),
                   ),
-                  Container(
-                    width: 150,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Fer-Win",
-                        textAlign: TextAlign.justify,
+                  SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Ferwin Lopez",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontFamily: 'Comfortaa',
+                            fontSize: 18,
+                            letterSpacing:
+                                0 /*percentages not used in flutter. defaulting to zero*/,
+                            fontWeight: FontWeight.w300,
+                            height: 1),
                       ),
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.black),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Adithya krishnan",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 0, 0, 0),
-                        fontFamily: 'Comfortaa',
-                        fontSize: 18,
-                        letterSpacing:
-                            0 /*percentages not used in flutter. defaulting to zero*/,
-                        fontWeight: FontWeight.w300,
-                        height: 1),
+                      Container(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Fer-Win",
+                            textAlign: TextAlign.justify,
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                  Container(
-                    width: 150,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        " fal3n-4ngel",
-                        textAlign: TextAlign.justify,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Adithya krishnan",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontFamily: 'Comfortaa',
+                            fontSize: 18,
+                            letterSpacing:
+                                0 /*percentages not used in flutter. defaulting to zero*/,
+                            fontWeight: FontWeight.w300,
+                            height: 1),
                       ),
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.black),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Liya Susan",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 0, 0, 0),
-                        fontFamily: 'Comfortaa',
-                        fontSize: 18,
-                        letterSpacing:
-                            0 /*percentages not used in flutter. defaulting to zero*/,
-                        fontWeight: FontWeight.w300,
-                        height: 1),
+                      Container(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            " fal3n-4ngel",
+                            textAlign: TextAlign.justify,
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                  Container(
-                    width: 150,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Liya-Susan",
-                        textAlign: TextAlign.justify,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Liya Susan",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontFamily: 'Comfortaa',
+                            fontSize: 18,
+                            letterSpacing:
+                                0 /*percentages not used in flutter. defaulting to zero*/,
+                            fontWeight: FontWeight.w300,
+                            height: 1),
                       ),
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.black),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Abhishek Raman",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 0, 0, 0),
-                        fontFamily: 'Comfortaa',
-                        fontSize: 18,
-                        letterSpacing:
-                            0 /*percentages not used in flutter. defaulting to zero*/,
-                        fontWeight: FontWeight.w300,
-                        height: 1),
+                      Container(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Liya-Susan",
+                            textAlign: TextAlign.justify,
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                  Container(
-                    width: 150,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "ice-weasel",
-                        textAlign: TextAlign.justify,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Abhishek Raman",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontFamily: 'Comfortaa',
+                            fontSize: 18,
+                            letterSpacing:
+                                0 /*percentages not used in flutter. defaulting to zero*/,
+                            fontWeight: FontWeight.w300,
+                            height: 1),
                       ),
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.black),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Christina Mathews",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 0, 0, 0),
-                        fontFamily: 'Comfortaa',
-                        fontSize: 18,
-                        letterSpacing:
-                            0 /*percentages not used in flutter. defaulting to zero*/,
-                        fontWeight: FontWeight.w300,
-                        height: 1),
+                      Container(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "ice-weasel",
+                            textAlign: TextAlign.justify,
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                  Container(
-                    width: 150,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "ChristinaMathews",
-                        textAlign: TextAlign.justify,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Christina Mathews",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontFamily: 'Comfortaa',
+                            fontSize: 18,
+                            letterSpacing:
+                                0 /*percentages not used in flutter. defaulting to zero*/,
+                            fontWeight: FontWeight.w300,
+                            height: 1),
                       ),
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.black),
-                      ),
-                    ),
+                      Container(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "ChristinaMathews",
+                            textAlign: TextAlign.justify,
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                          ),
+                        ),
+                      )
+                    ],
                   )
-                ],
-              )
-            ]),
+                ]),
           ),
         ));
   }
